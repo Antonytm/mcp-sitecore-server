6e5cb4fb429f7ef61cb8111c156b4e2e95ef16b4
Antonytm <antontishchenko@gmail.com>
2025-09-17 19:35:20 +0300

    feat: added configuration for MCP registry

--- tests/client.ts
+++ tests/client.ts
@@ -16,7 +16,7 @@ const transportOptions: TransportOptions = {
 
 const client = new Client({
     name: "mcp-sitecore-server",
-    version: "1.2.1",
+    version: "1.3.0",
 });
 
 const transport = createTransport(transportOptions);
