0988505ee91932c56d2cd23b5b6adb6c8006f300
Antonytm <antontishchenko@gmail.com>
2025-10-27 01:51:11 +0200

    feat: experiment with error

--- src/tools/powershell/simple/generic.ts
+++ src/tools/powershell/simple/generic.ts
@@ -12,15 +12,21 @@ export async function runGenericPowershellCommand(config: Config, command: strin
     );
 
     let text = ""
+    let isError = false;
     switch (outputFormat) {
         case PowerShellOutputType.JSON:
             text = await client.executeScriptJson(command, options);
+            const json1 = JSON.parse(text);
+            isError = json1?.Obj?.[0]?.ErrorCategory_Message !== undefined;
             break;
         case PowerShellOutputType.XML:
             text = await client.executeScript(command, options);
+            isError = text.includes("Error");
             break;
         default:
             text = await client.executeScriptJson(command, options);
+            const json2 = JSON.parse(text);
+            isError = json2?.Obj?.[0]?.ErrorCategory_Message !== undefined;
             break;
     }
 
@@ -31,7 +37,7 @@ export async function runGenericPowershellCommand(config: Config, command: strin
                 text: text,
             },
         ],
-        isError: false,
+        isError: isError,
     }
 
 }
